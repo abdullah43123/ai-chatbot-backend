5e5d186e6ee7ecb47c9487fa6739c9c9d90149cf
Abdullah Aslam <aabdullahaslam91@gmail.com>
2026-05-25 07:40:47 -0700

    GROK AI API

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.port
+const cors = require('cors')
+const Groq = require("groq-sdk");
+app.use(cors());
+app.use(express.json());
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY,
+});
+
+
+
+app.post("/chat", async (req, res) => {
+    try {
+        const { message } = req.body;
+
+        const chatCompletion = await groq.chat.completions.create({
+            messages: [
+                {
+                    role: "user",
+                    content: message,
+                },
+            ],
+            model: "llama-3.3-70b-versatile",
+        });
+
+        res.json({
+            success: true,
+            reply: chatCompletion.choices[0].message.content,
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
