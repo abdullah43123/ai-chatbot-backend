b39eb786a7a74972a7aef304b9d9024aa375e43c
Abdullah Aslam <aabdullahaslam91@gmail.com>
2026-05-25 08:22:18 -0700

    add vercel.json

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.port
 const cors = require('cors')
 const Groq = require("groq-sdk");
+
 app.use(cors());
 app.use(express.json());
 
 const groq = new Groq({
     apiKey: process.env.GROQ_API_KEY,
 });
-
-
 
 app.post("/chat", async (req, res) => {
     try {
@@ -34,17 +32,22 @@ app.post("/chat", async (req, res) => {
 
     } catch (error) {
         console.log(error);
-
         res.status(500).json({
             success: false,
             message: error.message,
         });
     }
 });
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    });
+}
